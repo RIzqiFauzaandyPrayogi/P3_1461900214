01ca3df9ef37cc12bc13560510a3e945d2d322ba
RIzqiFauzaandyPrayogi <rizqifauzaandyprayogi@gmail.com>
2021-06-08 21:09:27 +0700

    praktikum 3

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -2,17 +2,110 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\Buku;
 
 class BukuController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $buku = DB::table('buku')
-        ->select('.*')
-        ->get();
+        $buku = Buku::when($request->cari, function ($query) use ($request) {
+            $query
+            ->where('judul', 'like', "%{$request->cari}%");
+        })->paginate(5);
 
-    return view('buku', ['buku' => $buku]);
+        $buku->appends($request->only('cari'));
+
+        return view('buku.index', [
+            'buku' => $buku,
+        ])
+        ->with('i', ($request->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('buku.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'judul'        => 'required',
+            'tahun_terbit' => 'required',
+        ]);
+
+        Buku::create($request->all());
+
+        return redirect()
+                ->route('buku.index')
+                ->with('success','Buku created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Buku $buku)
+    {
+        return view('buku.show', compact('buku'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Buku $buku)
+    {
+        return view('buku.edit', compact('buku'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Buku $buku)
+    {
+        $request->validate([
+            'judul'        => 'required',
+            'tahun_terbit' => 'required',
+        ]);
+
+        $buku->update($request->all());
+
+        return redirect()
+                ->route('buku.index')
+                ->with('success','Buku updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Buku $buku)
+    {
+        $buku->delete();
+
+        return redirect()->route('buku.index')
+                ->with('success','Buku deleted successfully');
     }
 }
