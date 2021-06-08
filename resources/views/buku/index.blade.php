91b6c316797f4fe79b539e27043ec2bb069609dc
RIzqiFauzaandyPrayogi <rizqifauzaandyprayogi@gmail.com>
2021-06-08 23:48:33 +0700

    Praktikum 3

---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Buku</title>  
+</head>
+<body>
+    <table style="margin-left:auto;margin-right:auto" position="absolute" border="4" width="400px">
+        <thead>
+            <tr bgcolor="blue" align="center">
+            </tr>
+        </thead>
+        <tbody>@foreach($buku as $index => $value)
+            <tr>
+            </tr>
+        </tbody>@endforeach
+    </table>
+</body>
+</style>
+</html>
